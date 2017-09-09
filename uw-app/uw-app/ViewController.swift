fd93dc2f146b821ab79001c0057b1406f77b70a6
Zepeng An <z3an@uwaterloo.ca>
2017-09-09 17:44:34 -0400

    Cannot add stackview in background image

---
+++ uw-app/uw-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  uw-app
+//
+//  Created by 安泽鹏 on 2017-09-09.
+//  Copyright © 2017 zepengan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
