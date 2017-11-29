dd4d8fa7dfef20f71cf7b472cc7df0f5c1e9bc9f
Zepeng An <z3an@uwaterloo.ca>
2017-11-28 21:03:47 -0500

    add some function to course detail

--- uw-app/uw-app/CourseDetailsViewController.swift
+++ uw-app/uw-app/CourseDetailsViewController.swift
@@ -22,9 +22,11 @@ class CourseDetailsViewController: UIViewController, UITableViewDelegate, UITabl
         getSectionList()
         getSectionTitle(jsonData: self.classInfoList.array![0])
         getSectionCatalog(jsonData: self.classInfoList.array![0])
-        print(self.courseCatalog)
-        print(self.courseTitle)
-        print(self.descriptionData)
+        //print(self.courseCatalog)
+        //print(self.courseTitle)
+        //print(self.descriptionData)
+        //var tmp = getSectionInstructor(jsonData: self.classInfoList.array![0])
+        //print(tmp[1][1])
     }
 
     override func didReceiveMemoryWarning() {
@@ -53,10 +55,110 @@ class CourseDetailsViewController: UIViewController, UITableViewDelegate, UITabl
     {
         self.courseTitle = jsonData["title"].string!
     }
-    
-    // get the units of section
-    
-    
+    // get the units of section most of them are 0.5
+    func getSectionUnits(jsonData:JSON) -> String
+    {
+        return  String(jsonData["units"].double!)
+    }
+    // get the level of section
+    func getSectionLevel(jsonData:JSON) -> String
+    {
+        return jsonData["academic_level"].string!
+    }
+    // get course number of section
+    func getSectionCourseNumber(jsonData:JSON) -> String
+    {
+        return String(jsonData["class_number"].integer!)
+    }
+    // get notes of section
+    func getSectionNote(jsonData:JSON) -> String
+    {
+        return jsonData["note"].string!
+    }
+    // get campus of section
+    func getSectionCampus(jsonData:JSON) -> String
+    {
+        return jsonData["campus"].string!
+    }
+    // get associated class of section
+    func getSectionAssociatedClass(jsonData:JSON) -> String
+    {
+        return String(jsonData["associated_class"].integer!)
+    }
+    // get related_componet_1 of section
+    func getSectionRelatedComponent1(jsonData:JSON) -> String
+    {
+        return jsonData["related_component_1"].string!
+    }
+    // get related_componet_2 of section
+    func getSectionRelatedComponent2(jsonData:JSON) -> String
+    {
+        return jsonData["related_component_2"].string!
+    }
+    // get enrollment capacity of section
+    func getSectionEnrollmentCapacity(jsonData:JSON) -> String
+    {
+        return String(jsonData["enrollment_capacity"].integer!)
+    }
+    // get enrollment total of section
+    func getSectionEnrollmentTotal(jsonData:JSON) -> String
+    {
+        return String(jsonData["enrollment_total"].integer!)
+    }
+    // get wait cap
+    func getSectionWaitingCapacity(jsonData:JSON) -> String
+    {
+        return String(jsonData["waiting_capacity"].integer!)
+    }
+    // get wait total
+    func getSectionWaitingTotal(jsonData:JSON) -> String
+    {
+        return String(jsonData["waiting_total"].integer!)
+    }
+    // get time days time
+    func getSectionTime(jsonData:JSON) -> Array<String>
+    {
+        var courseTime = [String]()
+        let classes = jsonData["classes"].array!
+        for classData in classes{
+            if classData["date"]["is_tba"].bool!{
+                courseTime.append("TBA")
+            }
+            // is_cancelled  and is_closed
+            else{
+                courseTime.append(classData["date"]["start_time"].string! + "-" + classData["date"]["end_time"].string! + classData["date"]["weekdays"].string!)
+            }
+        }
+        return courseTime
+    }
+    // get location
+    func getSectionLocation(jsonData:JSON) -> Array<String>
+    {
+        var courseLocation = [String]()
+        let classes = jsonData["classes"].array!
+        for classData in classes{
+            if classData["date"]["is_tba"].bool!{}
+            else{
+                courseLocation.append(classData["location"]["building"].string! + " " + classData["location"]["room"].string!)
+            }
+        }
+        return courseLocation
+    }
+    // get instructors
+    func getSectionInstructor(jsonData:JSON) -> [[String]]
+    {
+        var courseInstructor = [[String]]()
+        let classes = jsonData["classes"].array!
+        for classData in classes{
+            let instructors = classData["instructors"].array!
+            var instructorsArray = [String]()
+            for instructor in instructors{
+                instructorsArray.append(instructor.string!)
+            }
+            courseInstructor.append(instructorsArray)
+        }
+        return courseInstructor
+    }
     
     /*
     // MARK: - Navigation
