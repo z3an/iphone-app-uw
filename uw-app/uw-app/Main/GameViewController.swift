e080d265113b7026ad3f03cbcff1d83d9d28ab9e
Zepeng An <z3an@uwaterloo.ca>
2017-12-07 00:50:32 -0500

    initialize game scene

--- uw-app/uw-app/Main/GameViewController.swift
+++ uw-app/uw-app/Main/GameViewController.swift
@@ -11,25 +11,15 @@ import SpriteKit
 import GameplayKit
 
 class GameViewController: UIViewController {
-    
+    var scene: GameScene!
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
-        }
+        let skView = view as! SKView
+        skView.isMultipleTouchEnabled = false
+        scene = GameScene(size: skView.bounds.size)
+        scene.scaleMode = .aspectFill
+        skView.presentScene(scene)
     }
     
     override var shouldAutorotate: Bool {
