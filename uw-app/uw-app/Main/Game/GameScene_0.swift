0b07788e9c75ad13dcd97895dd72feccb857c79c
Rain_Li <36seed@gmail.com>
2017-12-22 16:38:58 -0500

    fix UI issue; add default game

---
+++ uw-app/uw-app/Main/Game/GameScene_0.swift
@@ -0,0 +1,45 @@
+//
+//  GameScene.swift
+//  game
+//
+//  Created by 安泽鹏 on 2017-12-07.
+//  Copyright © 2017 uw-app. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        backgroundColor = UIColor.white
+    }
+    
+    
+    func touchDown(atPoint pos : CGPoint) {
+    }
+    
+    func touchMoved(toPoint pos : CGPoint) {
+    }
+    
+    func touchUp(atPoint pos : CGPoint) {
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
+
